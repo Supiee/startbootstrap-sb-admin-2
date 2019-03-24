564cc437ebeb3948a11f69cf0d0024354fb95615
Supiee <48849552+Supiee@users.noreply.github.com>
2019-03-24 23:01:13 +0530

    Create homeConroller1.js
    dummy controller

---
+++ js/homeConroller1.js
@@ -0,0 +1,18 @@
+mainApp.controller('homeController1', function($scope) {
+   $scope.student = {
+      firstName: "Mahesh",
+      lastName: "Parashar",
+      fees:500,
+      
+      subjects:[
+         {name:'Physics',marks:70},
+         {name:'Chemistry',marks:80},
+         {name:'Math',marks:65}
+      ],
+      fullName: function() {
+         var studentObject;
+         studentObject = $scope.student;
+         return studentObject.firstName + " " + studentObject.lastName;
+      }
+   };
+});
