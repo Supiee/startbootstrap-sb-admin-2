260463ae097de61b2cf83f928a6c07f7c9c8b920
Supiee <48849552+Supiee@users.noreply.github.com>
2019-03-24 23:01:28 +0530

    Update homeController.js
    changed

--- js/homeController.js
+++ js/homeController.js
@@ -1,18 +1,9 @@
 mainApp.controller('homeController', function($scope) {
-    $scope.student = {
-       firstName: "Mahesh",
-       lastName: "Parashar",
-       fees:500,
-       
-       subjects:[
-          {name:'Physics',marks:70},
-          {name:'Chemistry',marks:80},
-          {name:'Math',marks:65}
-       ],
-       fullName: function() {
-          var studentObject;
-          studentObject = $scope.student;
-          return studentObject.firstName + " " + studentObject.lastName;
-       }
-    };
- });
+            $scope.reset = function(){
+               $scope.firstName = "Mahesh";
+               $scope.lastName = "Parashar";
+               $scope.email = "MaheshParashar@tutorialspoint.com";
+            }
+            
+            $scope.reset();
+         });
